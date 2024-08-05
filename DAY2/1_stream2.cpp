86d859977f91ab86f1344c964e5a47b116079212
ecourse <smkang@codenuri.co.kr>
2024-08-06 08:41:05 +0900

    Add files via upload

--- DAY2/1_stream2.cpp
+++ DAY2/1_stream2.cpp
@@ -9,9 +9,11 @@ int main()
 	std::ostream_iterator<int> p1(std::cout, ", ");
 	std::istream_iterator<int> p2(std::cin);
 
-	int n = *p2; // std::cin >> n;
+//	int n = *p2; // std::cin >> n;
+//	std::cout << n << std::endl;
 
-	std::cout << n << std::endl;
+	// 위 2줄은 아래 처럼 표현 가능합니다.
+	*p1 = *p2;
 }
 
 
