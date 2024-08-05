ef2aec8323796033682926dd4545dc904144afff
ecourse <smkang@codenuri.co.kr>
2024-08-05 10:13:56 +0900

    Add files via upload

--- DAY1/3_CONTAINER2.cpp
+++ DAY1/3_CONTAINER2.cpp
@@ -9,6 +9,26 @@ template<typename T> struct Node
 	Node(const T& a, Node* n) : data(a), next(n) {}
 };
 
+// slist 의 요소를 가리키는 포인터 역활을 하는
+// 클래스를 만들어 봅시다.
+// "iterator" 라고 합니다.
+
+template<typename T> 
+class slist_iterator
+{
+
+public:
+
+};
+
+slist_iterator<int> p(500번지); // 1번째 요소의 주소를 보관했다가
+++p;		// 포인터와 동일한 방식으로 이동하고
+int n = *p; // 포인터와 동일한 방식으로 요소 접근
+
+
+
+
+
 template<typename T> class slist
 {
 	Node<T>* head = 0;
