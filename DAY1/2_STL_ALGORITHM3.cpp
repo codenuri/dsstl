6c25a9e41db6505b83f7c989418f34f904f9643b
ecourse <smkang@codenuri.co.kr>
2024-08-05 09:28:26 +0900

    Add files via upload

--- DAY1/2_STL_ALGORITHM3.cpp
+++ DAY1/2_STL_ALGORITHM3.cpp
@@ -15,10 +15,14 @@ T* find(T* first, T* last, T c)
 
 int main()
 {
-
 	double x[] = { 1,2,3,4,5,6,7,8,9,10 };
 
+	// 주의 10 개 배열의 마지막 요소는 x+9 이지만
+	// => x, x+9 로 전달하면 마지막 요소는 검색 대상 아님
+	// => 전체 검색 하려면 마지막 다음요소의 주소 전달(x+10)
+
 	double* p = find(x, x + 10, 5.0);
+
 
 	if (p == nullptr)
 		std::cout << "not found" << std::endl;
