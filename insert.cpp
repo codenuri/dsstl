51ebf8633b09a68775b0efa86ed644166c60c4a4
ecourse <smkang@codenuri.co.kr>
2024-08-05 20:32:10 +0900

    Add files via upload

--- insert.cpp
+++ insert.cpp
@@ -5,14 +5,6 @@
 template<typename C> class my_back_insert_iterator
 {
 public:
-	using iterator_category = std::output_iterator_tag;
-	using value_type = void;
-	using difference_type = void;
-	using pointer = void;
-	using reference = void;
-
-	using container_type = C;
-
 	explicit my_back_insert_iterator(C& _Cont)
 		: container(_STD addressof(_Cont))
 	{	// construct with container
@@ -21,12 +13,6 @@ public:
 	my_back_insert_iterator& operator=(const typename C::value_type& _Val)
 	{	// push value into container
 		container->push_back(_Val);
-		return (*this);
-	}
-
-	my_back_insert_iterator& operator=(typename C::value_type&& _Val)
-	{	// push value into container
-		container->push_back(_STD move(_Val));
 		return (*this);
 	}
 
@@ -39,7 +25,6 @@ public:
 	{	// pretend to preincrement
 		return (*this);
 	}
-
 	my_back_insert_iterator operator++(int)
 	{	// pretend to postincrement
 		return (*this);
@@ -61,20 +46,8 @@ int main()
 {
 	std::list<int> s1 = { 1, 2, 3, 4, 5 };
 	std::list<int> s2 = { 1, 2, 3, 4, 5 };
-
-	auto p = my_back_inserter(s2);
-
-	*p = 10;
-	++p;     // 아무일도 하지 않음.
-	// 하지만, 반드시 지원해야 한다.
-	// 그래야, std::copy 같은 곳에 보낼수 있다.
-	*p = 20;
-
-	std::copy(s1.begin(), s1.end(), p); // ??
-	// => std::copy 는 내부적으로 1번째, 3번째 인자로 받은
-	//    반복자를 ++ 연산으로 이동하게 됩니다.
-	// => 따라서, 3번째 인자 p 는 반드시 ++연산을 제공해야합니다
-
+		
+	std::copy(s1.begin(), s1.end(), my_back_inserter(s2));
 
 	for (auto e : s2)
 	{
