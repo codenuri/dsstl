787e0ad1dbfc48389bd3c3a1e532db66def5cd32
ecourse <smkang@codenuri.co.kr>
2024-08-05 12:42:11 +0900

    Add files via upload

--- DAY1/4_typename2.cpp
+++ DAY1/4_typename2.cpp
@@ -9,10 +9,20 @@ void print_first_element(std::vector<int>& v)
 	std::cout << n << std::endl;
 }
 */
-
+/*
 template<typename T>
 void print_first_element(std::vector<T>& v)
 {
+	T n = v.front();
+
+	std::cout << n << std::endl;
+}
+*/
+template<typename T>
+void print_first_element(T& v)
+{
+	// T : std::list<double>
+
 	? n = v.front();
 
 	std::cout << n << std::endl;
@@ -22,7 +32,23 @@ void print_first_element(std::vector<T>& v)
 int main()
 {
 //	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
-	std::vector<double> v = { 1,2,3,4,5,6,7,8,9,10 };
+//	std::vector<double> v = { 1,2,3,4,5,6,7,8,9,10 };
+	std::list<double> v = { 1,2,3,4,5,6,7,8,9,10 };
 
 	print_first_element(v);
 }
+
+// template 기반 컨테이너 설계시
+// 컨테이너가 저장하는 타입을 알고 싶을때가 있습니다.
+// 위 "print_first_element" 처럼
+
+// 그래서 STL 은 아래 처럼 설계되었습니다.
+template<typename T>
+class list
+{
+public:
+	using value_type = T; // 핵심!!!
+};
+
+list<int> s = { 1,2,3 };
+list<int>::value_type n = s.front(); 
