3ce49b1095b9b1131e916c82e22532c4cb23cc10
ecourse <smkang@codenuri.co.kr>
2024-08-05 17:01:30 +0900

    Add files via upload

--- DAY1/6_TRAITS4.cpp
+++ DAY1/6_TRAITS4.cpp
@@ -5,12 +5,22 @@ template<typename C>
 void f1(C& c)
 {
 	// ? 채우세요
-	? n = c.front();
+	typename C::value_type n = c.front();
+	// => 컨테이너가 보관하는 요소의 타입
 }
 template<typename IT>
-void f2(IT& p)
+void f2(IT p)
 {
-	? n = *p;
+	typename IT::value_type n = *p; 
+				// 반복자 IT 가 가리키는 타입
+				// 단, 객체형 반복자만 가능. raw pointer 안됨.
+
+	typename std::iterator_traits<IT>::value_type n2 = *p;
+				// 객체형 반복자, raw pointer 모두 가능
+
+	// C++20 부터 위코드의 별명이 제공됩니다.
+	std::iter_value_t<IT>  n3 = *p; // typename 필요 없습니다.
+									// :: 이 없습니다.
 }
 
 int main()
