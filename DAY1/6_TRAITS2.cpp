7de8ca764d31c0c7ad355add79c8370a6f9192c2
ecourse <smkang@codenuri.co.kr>
2024-08-05 16:41:27 +0900

    Add files via upload

---
+++ DAY1/6_TRAITS2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+
+
+template<typename T>
+typename T::value_type sum(T first, T last)
+{
+	typename T::value_type  s = 0;
+
+	while (first != last)
+	{
+		s += *first;
+		++first;
+	}
+	return s;
+}
+
+int main()
+{
+	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
+	int n = sum(s.begin(), s.end());
+	std::cout << n << std::endl; 
+
+
+	// 핵심 : raw pointer 도 반복자 입니다.
+	int x[10] = {1,2,3,4,5,6,7,8,9,10};
+
+	int n1 = sum(x, x + 10);
+
+	std::cout << n1 << std::endl; 
+
+}
+
