1b3195e8f2dbf823235c80dd03a1c1ff1b43bcc1
ecourse <smkang@codenuri.co.kr>
2024-08-06 08:46:44 +0900

    Add files via upload

--- DAY2/1_stream3.cpp
+++ DAY2/1_stream3.cpp
@@ -5,14 +5,15 @@
 
 int main()
 {
-	std::ostream_iterator<int> p1(std::cout, ", ");
-	std::istream_iterator<int> p2(std::cin);
+//	std::ostream_iterator<int> p1(std::cout, ", ");
+//	std::istream_iterator<int> p2(std::cin);
 
 	std::ifstream fin("1_stream3.cpp"); // 현재 파일이름
 
 	std::istream_iterator<char> p3(fin);
 
-	std::cout << *p3 << std::endl;
+	std::cout << *p3 << std::endl; // #
+	++p3;
 	std::cout << *p3 << std::endl;
 }
 
