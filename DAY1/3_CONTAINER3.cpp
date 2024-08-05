b91302861e30be634effe8f93d93643dacd3389a
ecourse <smkang@codenuri.co.kr>
2024-08-05 10:37:52 +0900

    Add files via upload

---
+++ DAY1/3_CONTAINER3.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+
+template<typename T> struct Node
+{
+	T     data;
+	Node* next;
+
+	Node(const T& a, Node* n) : data(a), next(n) {}
+};
+
+
+template<typename T>
+class slist_iterator
+{
+	Node<T>* current;
+public:
+	slist_iterator(Node<T>* p = nullptr)
+		: current(p) {}
+
+	inline slist_iterator& operator++()
+	{
+		current = current->next; 
+		return *this;
+	}
+
+	inline T& operator*() { return current->data; }
+
+	inline bool operator==(const slist_iterator& other)
+	{
+		return current == other.current;
+	}
+	inline bool operator!=(const slist_iterator& other)
+	{
+		return current != other.current;
+	}
+};
+
+
+
+
+
+template<typename T> class slist
+{
+	Node<T>* head = 0;
+public:
+	void push_front(const T& a)
+	{
+		head = new Node<T>(a, head);
+	}
+
+	// 규칙 : 모든 컨테이너(자료구조를 구현한 클래스)
+	//       는 자신의 반복자 이름을 약속된 형태인 "iterator" 로
+	//       외부 노출해야 합니다.
+
+	using iterator = slist_iterator<T>;
+					// typedef slist_iterator<T> iterator 의미
+
+
+	slist_iterator<T> begin() { return slist_iterator<T>(head); }
+	slist_iterator<T> end() { return slist_iterator<T>(0); }
+};
+
+int main()
+{
+	slist<int> s;
+
+	s.push_front(10);
+	s.push_front(20);
+	s.push_front(30);
+	s.push_front(40);
+	s.push_front(50);
+
+	slist_iterator<int> p1 = s.begin();
+	slist_iterator<int> p2 = s.end();  
+
+	while (p1 != p2)
+	{
+		std::cout << *p1 << std::endl;
+		++p1;
+	}
+}
+
