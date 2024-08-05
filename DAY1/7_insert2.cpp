fa39c1df1d266529087c3f379b967963a278198f
ecourse <smkang@codenuri.co.kr>
2024-08-05 15:34:59 +0900

    Add files via upload

--- DAY1/7_insert2.cpp
+++ DAY1/7_insert2.cpp
@@ -8,10 +8,15 @@
 int main(int argc, char** argv)
 {
 	std::list<int> s1 = { 10,20,30,40,50 };
-
 	std::list<int> s2 = { 1, 2, 3, 4, 5 };
 	
-	std::copy(s1.begin(), s1.end(), s2.begin());
+	// #1. 아래 코드는 s1의 내용을 s2에 "덮어쓰기"
+//	std::copy(s1.begin(), s1.end(), s2.begin());
+
+	// #2. 
+	std::back_insert_iterator<std::list<int>> p(s2);
+
+	std::copy(s1.begin(), s1.end(), p );
 
 
 	for (auto& n : s2)
