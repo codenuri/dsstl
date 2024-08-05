c70b4464b2fdcbc7aa3c655461dd1a7a57ee8915
ecourse <smkang@codenuri.co.kr>
2024-08-05 15:48:38 +0900

    Add files via upload

--- DAY1/7_insert3.cpp
+++ DAY1/7_insert3.cpp
@@ -16,7 +16,13 @@ int main(int argc, char** argv)
 	std::insert_iterator< std::list<int> > p3(s2,		// 컨테이너
 							std::next(s2.begin(), 2) );	// 삽입할 위치
 
-	std::copy(s1.begin(), s1.end(), p3);
+//	std::copy(s1.begin(), s1.end(), p3); // 0, 0, 1,2,3,4,5,0,0,0
+
+	// #2. 함수 버전
+	std::copy(s1.begin(), s1.end(), std::back_inserter(s2));
+	std::copy(s1.begin(), s1.end(), std::front_inserter(s2));
+	std::copy(s1.begin(), s1.end(), std::inserter(s2, s2.begin()));
+
 
 	for (auto& n : s2)
 		std::cout << n << ", ";
