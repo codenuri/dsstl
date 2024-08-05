832ccb911f8b776207f98a3c0efe34ff3b9d1cf3
ecourse <smkang@codenuri.co.kr>
2024-08-05 13:35:14 +0900

    Add files via upload

---
+++ DAY1/5_iterator6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::list<int> s1 = { 1, 2, 3, 4, 5 };
+	std::list<int> s2 = { 0, 0, 0, 0, 0 };
+
+	// s1의 모든 요소를 s2에 복사해 보세요
+}
