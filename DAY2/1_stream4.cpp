4ecfe22b989106a13183c587b8cd3d2f00cf8b8c
ecourse <smkang@codenuri.co.kr>
2024-08-06 08:53:09 +0900

    Add files via upload

--- DAY2/1_stream4.cpp
+++ DAY2/1_stream4.cpp
@@ -7,11 +7,16 @@ int main()
 {
 	std::ifstream fin("1_stream3.cpp"); // 현재 파일이름
 
-	std::istream_iterator<char> p1(fin); 
-	std::istream_iterator<char> p2;		// 인자가 없으면 EOF
+	// std::istream_iterator : white space 입력안됨(space,tab, enter)
+	// std::istreambuf_iterator : white space 입력됨
+//	std::istream_iterator<char> p1(fin); 
+//	std::istream_iterator<char> p2;		// 인자가 없으면 EOF
+
+	std::istreambuf_iterator<char> p1(fin); 
+	std::istreambuf_iterator<char> p2;		
+
 
 	std::ostream_iterator<char> p3(std::cout, "");
-
 
 	std::copy(p1, p2, p3);
 }
---
+++ DAY2/1_stream5.cpp
@@ -0,0 +1,17 @@
+// github.com/codenuri/dsstl  에서 DAY2.zip  받으시면 됩니다.
+
+#include <iostream>
+#include <iterator>
+#include <list>
+
+int main()
+{
+	std::list<int> s = { 1, 2, 3 };
+	int x[3] = { 1, 2, 3 };
+
+	show(s);
+	show(x);
+
+}
+
+
