1fbde739161297c84460d6e374f8677d817690f5
ecourse <smkang@codenuri.co.kr>
2024-08-05 13:39:04 +0900

    Add files via upload

--- DAY1/5_iterator6.cpp
+++ DAY1/5_iterator6.cpp
@@ -8,4 +8,35 @@ int main()
 	std::list<int> s2 = { 0, 0, 0, 0, 0 };
 
 	// s1의 모든 요소를 s2에 복사해 보세요
+	// 
+	// #1. [] 연산자와 for 문 - list 는 안됩니다.
+	for (int i = 0; i < 5; i++)
+	{
+//		s2[i] = s1[i];	// error. list 는 [] 연산 안됨
+						// vector 만 [] 연산 가능
+						// 이유는 3일차에
+	}
+
+	// #2. 반복자로 해야 합니다.
+	auto p1 = s1.begin();
+	auto p2 = s1.end();
+
+	auto p3 = s2.begin();
+
+	while (p1 != p2)
+	{
+		*p3 = *p1;
+		++p1;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
