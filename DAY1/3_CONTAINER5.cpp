6dac362706e5db4515fe9399eb0e975d8a1556fe
ecourse <smkang@codenuri.co.kr>
2024-08-05 12:27:10 +0900

    Add files via upload

--- DAY1/3_CONTAINER5.cpp
+++ DAY1/3_CONTAINER5.cpp
@@ -6,13 +6,19 @@
 					 // 이 헤더에 있습니다.
 int main()
 {
-	std::list<int> c = { 1,2,3,4,5,6,7,8,9,10 };
+//	std::list<int>   c = { 1,2,3,4,5,6,7,8,9,10 };
+	std::vector<int> c = { 1,2,3,4,5,6,7,8,9,10 };
 
 	// 아래 코드에서 ? 채워 보세요
-	? p = std::find(? , ? , 5 ); 
+	std::list<int>::iterator p = std::find( c.begin(), c.end(), 5);
+								 // c.find(5)
 
-	if (p == ? )
+	if (p == c.end() )
 		std::cout << "검색실패\n";
 	else
-		std::cout << "검색성공 : " << ? << std::endl;
+		std::cout << "검색성공 : " << *p << std::endl;
 }
+
+// 1일차 : iterator
+// 2일차 : algorithm
+// 3일차 : container 
