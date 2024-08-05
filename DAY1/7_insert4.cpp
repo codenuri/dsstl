86b310af21d6356179535b67024c653aa5f44ca9
ecourse <smkang@codenuri.co.kr>
2024-08-05 16:01:41 +0900

    Add files via upload

--- DAY1/7_insert4.cpp
+++ DAY1/7_insert4.cpp
@@ -2,14 +2,34 @@
 #include <list>
 #include <iterator>
 
+template<typename T>
+class my_back_insert_iterator
+{
+	T& c;
+public:
+	my_back_insert_iterator(T& c) : c(c) {}
+
+	my_back_insert_iterator& operator*() 
+	{
+		return *this; 
+	}
+	void operator=( const typename T::value_type& value)
+	{
+		c.push_back(value);
+	}
+};
 int main()
 {
 	std::list<int> s = { 1, 2, 3, 4, 5 };
 
-	std::back_insert_iterator< std::list<int> > p(s);
-
+	my_back_insert_iterator< std::list<int> > p(s);
 
 	*p = 10; // s.push_back(10)
-
-
+			 // ( p.operator*() ).operator=(10)
+			 // p.operator=(10)
+	
+	for (auto e : s)
+	{
+		std::cout << e << ", ";
+	}
 }
