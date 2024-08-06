4a92e1707d9a7ea83ae0571e48b6482f2eb4d0f3
ecourse <smkang@codenuri.co.kr>
2024-08-07 08:47:32 +0900

    Add files via upload

--- DAY3/1_단위전략3.cpp
+++ DAY3/1_단위전략3.cpp
@@ -1,5 +1,14 @@
 #include <memory> // std::allocator 사용하기 위한 헤더
 
+// 단위전략 디자인(Policy Base Design)
+// => 클래스가 사용하는 "어떤 정책" 을
+//    별도의 클래스로 설계해서 템플릿 인자로 교체 할수 있게 한다.
+
+// => 성능 저하 없이 "정책을 교체" 하는 기술
+
+// => STL 만들때 사용 했던 핵심 기법. 
+
+
 template<typename T,							// 저장할 요소의 타입
 	     typename Alloc = std::allocator<T> >   // 메모리 할당기
 class vector
