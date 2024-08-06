d13b7afcfc9541a97fdaebb9d23d0fcc44ebc172
ecourse <smkang@codenuri.co.kr>
2024-08-06 16:01:12 +0900

    Add files via upload

--- DAY2/8_constraint_algorithm02.cpp
+++ DAY2/8_constraint_algorithm02.cpp
@@ -18,9 +18,24 @@ int main()
 
 	// 문자열의 갯수를 기준으로 정렬해 보세요
 
+	// C++98 : 비교 함수의 인자가 2개 이다.
 	std::sort(v.begin(), v.end(), 
+
 			[](const std::string& s1, const std::string& s2) 
 			{ return s1.size() < s2.size(); } );
+
+	// PYTHON : 비교 함수의 인자가 한개 이다.
+	// => 2개의 요소를 비교할때
+	// => 비교함수(1st) < 비교함수(2nd) 로 비교
+
+	/*
+	std::sort(v.begin(), v.end(),
+		[](const std::string& s) { return s1.size(); } );
+	*/
+
+	// C++20 : 위 2개 개념을 합쳐서 설계 했습니다.
+	// => 다음 소스에서
+
 
 
 	show(v);
