f6dbae6840209ec4960e916188b88fdb88155364
ecourse <smkang@codenuri.co.kr>
2024-08-06 15:01:35 +0900

    Add files via upload

--- DAY2/6_알고리즘의4가지변형2.cpp
+++ DAY2/6_알고리즘의4가지변형2.cpp
@@ -14,7 +14,13 @@ int main()
 	// 위 코드를 참고해서 아래 주석 처럼 동작하는 코드를 만들어 보세요
 
 	// [v1.begin(), v1.end() ) 에서 "홀수를 0으로 변경한 결과를"
-	// v2 에 넣어 보세요
+	// v2 에 넣어 보세요 - lambda 표현식으로!
+
+	std::replace_copy_if(v1.begin(), v1.end(), 
+
+			std::back_inserter(v2),
+
+			[](int n) { return n % 2 == 1; }, 0);
 
 
 	show(v2); // 0, 2, 0, 4, 0, 6, 0, 8, 0, 10
