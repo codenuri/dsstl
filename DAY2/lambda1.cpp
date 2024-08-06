e01881153c05358f0e53568c22ac1185ad7f23b6
ecourse <smkang@codenuri.co.kr>
2024-08-06 14:15:20 +0900

    Add files via upload

--- DAY2/lambda1.cpp
+++ DAY2/lambda1.cpp
@@ -1,4 +1,4 @@
-// lambda1.cpp - 함수객체8.cpp 복사
+// lambda1.cpp
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -10,7 +10,16 @@ int main()
 
 	std::sort(v.begin(), v.end(), [](int a, int b) {return a < b; });
 
-
+	// 위 한줄을 컴파일러는 아래 처럼 변경합니다.
+	class CompilerGeneratedName
+	{
+	public:
+		inline bool operator()(int a, int b)
+		{
+			return a < b;
+		}
+	};
+	std::sort(v.begin(), v.end(), CompilerGeneratedName{} );
 
 }
 
