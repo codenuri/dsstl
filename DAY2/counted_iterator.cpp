e6fd9c2b2e0fce41e61b9ad8af5397b942745abd
ecourse <smkang@codenuri.co.kr>
2024-08-06 16:52:14 +0900

    Add files via upload

--- DAY2/counted_iterator.cpp
+++ DAY2/counted_iterator.cpp
@@ -3,6 +3,26 @@
 #include <algorithm>
 #include <iterator>
 
+template<typename IT>
+class counted_iterator
+{
+	IT iter;
+	int length;
+public:
+	counted_iterator(IT it, int cnt)
+		: iter(it), length(cnt) {}
+
+	int count() { return length; }
+
+	counted_iterator& operator++()
+	{
+		++iter;
+		--length;
+		return *this;
+	}
+};
+
+
 int main()
 {
 	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
