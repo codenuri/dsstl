18392e88ac7119e62fdeeadb48ff177260d0c5d0
ecourse <smkang@codenuri.co.kr>
2024-08-06 17:03:31 +0900

    Add files via upload

--- DAY2/counted_iterator.cpp
+++ DAY2/counted_iterator.cpp
@@ -3,6 +3,19 @@
 #include <algorithm>
 #include <iterator>
 
+class my_default_sentinel_t
+{
+};
+my_default_sentinel_t my_default_sentinel;
+
+// my_default_sentinel_t
+// => 어떠한 멤버도 없는 클래스 입니다.(empty class)
+// => empty 라도 분명 한나의 독립된 타입입니다
+// => 참고로 empty class 의 크기는 1byte 입니다.
+
+
+
+
 template<typename IT>
 class my_counted_iterator
 {
@@ -20,9 +33,22 @@ public:
 		return *this;
 	}
 
-	typename std::iterator_traits<T>::value_type& operator*()
+	typename std::iterator_traits<IT>::value_type& operator*()
 	{ 
 		return *iter; 
+	}
+
+	bool operator!=(my_default_sentinel_t a)
+	{
+		// a 인자도 필요 없습니다. 
+		// counted_iterator 는 자신이  스스로 끝인지 판단가능합니다.
+		return length != 0;
+	}
+	bool operator==(my_default_sentinel_t a)
+	{
+		// a 인자도 필요 없습니다. 
+		// counted_iterator 는 자신이  스스로 끝인지 판단가능합니다.
+		return length == 0;
 	}
 };
 
@@ -32,8 +58,8 @@ int main()
 
 	my_counted_iterator ci(s.begin(), 5);
 
-//	while (ci != std::default_sentinel)
-	while (ci.count() != 0)
+	while (ci != my_default_sentinel) // ci.operator!=(my_default_sentinel_t)
+//	while (ci.count() != 0)
 	{
 		std::cout << *ci << std::endl;
 		++ci;
