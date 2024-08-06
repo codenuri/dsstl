be907effe983dd7c839a069f7039f7cff3814f6d
ecourse <smkang@codenuri.co.kr>
2024-08-06 10:32:42 +0900

    Add files via upload

--- DAY2/3_algorithm5.cpp
+++ DAY2/3_algorithm5.cpp
@@ -16,11 +16,20 @@ int main()
 	        // 2. BB, CC, , 
 			// 3. BB, CC, AA,   <= 정답
 
+	// std::remove 는 뒤의 요소를 앞으로 이동할때
+	// => std::move 사용합니다.
+	// => v[1] = std::move(v[3])
+	// => v[1] = std::move("CC") 에서 
+ 	//    CC 문자열이 std::move 되므로 문자열 자원만 빼앗기게 된것
+
+
 	// 위 코드를 이해 하려면 std::move 알아야 합니다.
 	std::string s1 = "AA";
 	std::string s2 = "AA";
-	std::string s3 = s1;
-	std::string s4 = s2;
+
+	std::string s3 = s1;	// s1의 문자열을 s3으로 복사
+	std::string s4 = std::move(s2); // s2 문자열을 s4로 이동
+								    // s2 는 더이상 자원 없음
 }
 
 
