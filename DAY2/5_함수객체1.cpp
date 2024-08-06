e5c4d0d917c0652f2f256de6f937f791d1d7d18d
ecourse <smkang@codenuri.co.kr>
2024-08-06 12:29:17 +0900

    Add files via upload

--- DAY2/5_함수객체1.cpp
+++ DAY2/5_함수객체1.cpp
@@ -1,5 +1,14 @@
 // 3_함수객체1
 #include <iostream>
+// function object
+// => () 연산자 함수를 제공해서 함수 처럼 사용가능한 객체
+
+// 왜?? 일반 함수를 만들면 되는데, 함수 객체를 사용하나요 ?
+
+// 장점이 너무 많습니다
+// => 상태를 가지는 함수, closure!
+// => 특정 상황에서 일반 함수보다 함수객체가 빠릅니다.
+
 
 struct Plus
 {
@@ -17,6 +26,7 @@ int main()
 					 // => 함수객체(function object) 라고 합니다.
 				
 	int n2 = p.operator()(1, 2); // 이렇게 직접 호출해도 됩니다.
+
 
 	// a + b; // a.operator+(b)
 	// a - b; // a.operator-(b)
