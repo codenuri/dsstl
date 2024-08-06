be248ff864b6938eb7fc15ecd60d3e755ef942e5
ecourse <smkang@codenuri.co.kr>
2024-08-06 14:35:55 +0900

    Add files via upload

---
+++ DAY2/lambda5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+
+bool foo(int n) { return n % 3 == 0; }
+
+
+class IsDivide
+{
+	int value;
+public:
+	IsDivide(int n) : value(n) { } 
+
+	bool operator()(int n) { return n % value == 0; }
+};
+
+
+int main()
+{
+	std::list<int> s = { 1,2,6,4,5,3,7,8,9,10 };
+
+	int k = 3;
+
+	auto ret1 = std::find_if(s.begin(), s.end(), IsDivide{ k });
+
+	
+}
+
+
+
+
