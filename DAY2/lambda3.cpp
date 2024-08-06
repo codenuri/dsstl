c296ea1b81e6ec03f5c5f35e12d6c99dd1dec30b
ecourse <smkang@codenuri.co.kr>
2024-08-06 14:26:51 +0900

    Add files via upload

--- DAY2/lambda3.cpp
+++ DAY2/lambda3.cpp
@@ -13,8 +13,13 @@ int main()
 			// class BBB{}; BBB{}
 
 	// f1, f2 는 "같은 타입" 일까요 ? "다른 타입" 일까요 ?
-
+	// => 다른 타입
 	std::cout << typeid(f1).name() << std::endl;
 	std::cout << typeid(f2).name() << std::endl;
+
+
+	// #2. 람다 표현식으로 초기화된 auto 변수에 다른 람다표현식을
+	// 담을수 없습니다
+	f2 = [](int a, int b) { return a + b; }; // error
 
 }
