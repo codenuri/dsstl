86945a5dd3c34b0ae44d9627eabc5cf0027be7f4
ecourse <smkang@codenuri.co.kr>
2024-08-06 15:54:30 +0900

    Add files via upload

---
+++ DAY2/8_constraint_algorithm02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <functional>
+#include "show.h"
+
+// projection 개념
+
+int main()
+{
+	std::vector<std::string> v = { "AAAA", "DD", "C", "BBBBB" };
+	
+//	std::sort( v.begin(), v.end() );
+				// => "<" 연산으로 정렬
+				// => 사전순서로 정렬
+
+	// 문자열의 갯수를 기준으로 정렬해 보세요
+
+	std::sort(v.begin(), v.end());
+
+
+	show(v);
+}
+
+
+
+
