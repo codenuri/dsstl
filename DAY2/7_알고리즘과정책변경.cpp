99b767aa13be7c7692776359578db9f77e6000b4
ecourse <smkang@codenuri.co.kr>
2024-08-06 15:28:29 +0900

    Add files via upload

--- DAY2/7_알고리즘과정책변경.cpp
+++ DAY2/7_알고리즘과정책변경.cpp
@@ -15,11 +15,26 @@
 // <memory>
 // => 메모리 관련, 요소 초기화등..
 
+int mul(int a, int b) { return a * b; }
+
 int main()
 {
 	std::vector<int> v1 = { 1,2,3,4,5,6,7,8,9,10 };
 
-	int n1 = std::accumulate(v1.begin(), v1.end(), 0);
+//	int n1 = std::accumulate(v1.begin(), v1.end(), 0);
+							// => 이항함수 생략시 "+" 로 수행.
+
+//	int n1 = std::accumulate(v1.begin(), v1.end(), 1, mul);
+							// => 이항 함수 전달 버전. 
+							// => 일반 함수 전달했으므로 인라인 치환 안됨
+
+
+	int n1 = std::accumulate(v1.begin(), v1.end(), 1, 
+										std::multiplies<int>{} );
+							// => 함수 객체 버전
+							// => +,-,*,/ 등의 연산을 수행하는
+							//    함수 객체가 이미 표준에 있습니다.
+							//    cppreference.com 참고
 
 	std::cout << n1 << std::endl;
 
---
+++ DAY2/7_알고리즘과정책변경2.cpp
@@ -0,0 +1,20 @@
+// 3_알고리즘과정책변경
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>    // 수치 관련 연산
+#include <functional>
+#include "show.h"
+
+
+int main()
+{
+	std::vector<int> v1 = { 1,2,3,4,5,6,7,8,9,10 };
+	std::vector<int> v2;
+
+
+	std::partial_sum(v1.begin(), b1.end(), std::back_inserter(v2));
+
+
+	show(v2);
+}
