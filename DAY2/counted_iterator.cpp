da9ada33ca1748c82049f2869e62f0c997089e4c
ecourse <smkang@codenuri.co.kr>
2024-08-06 16:55:37 +0900

    Add files via upload

--- DAY2/counted_iterator.cpp
+++ DAY2/counted_iterator.cpp
@@ -4,33 +4,36 @@
 #include <iterator>
 
 template<typename IT>
-class counted_iterator
+class my_counted_iterator
 {
 	IT iter;
 	int length;
 public:
-	counted_iterator(IT it, int cnt)
-		: iter(it), length(cnt) {}
+	my_counted_iterator(IT it, int cnt) : iter(it), length(cnt) {}
 
 	int count() { return length; }
 
-	counted_iterator& operator++()
+	my_counted_iterator& operator++()
 	{
 		++iter;
 		--length;
 		return *this;
 	}
-};
 
+	typename std::iterator_traits<T>::value_type& operator*()
+	{ 
+		return *iter; 
+	}
+};
 
 int main()
 {
 	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
 
+	my_counted_iterator ci(s.begin(), 5);
 
-	std::counted_iterator ci(s.begin(), 5);
-
-	while (ci != std::default_sentinel)
+//	while (ci != std::default_sentinel)
+	while (ci.count() != 0)
 	{
 		std::cout << *ci << std::endl;
 		++ci;
