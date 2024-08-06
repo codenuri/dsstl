dd321c4d930785b9c168a79b6b6f5fb270cd2c63
ecourse <smkang@codenuri.co.kr>
2024-08-06 10:45:40 +0900

    Add files via upload

--- DAY2/4_알고리즘과함수2.cpp
+++ DAY2/4_알고리즘과함수2.cpp
@@ -4,6 +4,8 @@
 #include "show.h"
 
 
+int foo(int a, int b) { return a + b; }
+
 int main()
 {
     std::list<int> s1 = { 1,2,3,4,5};
@@ -14,7 +16,8 @@ int main()
 
     std::transform(s1.begin(), s1.end(), // 1. 이구간의 요소를
                    s2.begin(),           // 2. 이구간의 요소와 함께
-                   s3.begin(),           // 4. 이구간에 넣어라
+                 //s3.begin(),           // 4. 이구간에 넣어라
+                   std::back_inserter(s3),
                    foo);                 // 3. 이함수에보내서 결과를
 
     
