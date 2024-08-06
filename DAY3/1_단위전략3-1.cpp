dfc2ae84ac8291a1c792aa69ca2e6e8331afea56
ecourse <smkang@codenuri.co.kr>
2024-08-07 08:52:06 +0900

    Add files via upload

--- DAY3/1_단위전략3-1.cpp
+++ DAY3/1_단위전략3-1.cpp
@@ -2,9 +2,22 @@
 #include <vector>
 #include <iostream>
 
+// 사용자 정의 메모리 할당기
+
+// 1. 반드시 템플릿일 필요는 없지만, 대부분 다양한 타입에 대해
+//    메모리 할당해야 하므로 관례상 템플릿으로
+
+template<typename T>
+class debug_alloc
+{
+public:
+
+};
+
+
 int main()
 {
-	std::vector<int> v;
+	std::vector<int, debug_alloc<int> > v;
 
 	std::cout << "------------------\n";
 
