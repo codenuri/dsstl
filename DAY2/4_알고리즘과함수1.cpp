c9eb55b683482a29ae6386e692cfe416df2af01e
ecourse <smkang@codenuri.co.kr>
2024-08-06 10:36:07 +0900

    Add files via upload

---
+++ DAY2/4_알고리즘과함수1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "show.h"
+using namespace std;
+
+
+// STL 의 많은 알고리즘이 인자로 함수를 요구 합니다
+
+// 단항함수(unary function)  : 인자가 1개인 함수
+// 이항함수(binary function) : 인자가 2개인 함수
+void foo(int n)
+{
+    std::cout << n << std::endl;
+}
+
+int main()
+{
+    std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
+    
+    // for_each 알고리즘
+    std::for_each(v.begin(), v.end(), foo);
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
