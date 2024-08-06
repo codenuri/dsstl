97f02da6cb85969f19f941b16ae4746d7ca37370
ecourse <smkang@codenuri.co.kr>
2024-08-06 10:41:01 +0900

    Add files via upload

--- DAY2/4_알고리즘과함수1.cpp
+++ DAY2/4_알고리즘과함수1.cpp
@@ -9,17 +9,21 @@ using namespace std;
 
 // 단항함수(unary function)  : 인자가 1개인 함수
 // 이항함수(binary function) : 인자가 2개인 함수
-void foo(int n)
-{
-    std::cout << n << std::endl;
-}
+
+void foo(int n) {   std::cout << n << std::endl;}
 
 int main()
 {
     std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
     
-    // for_each 알고리즘
+    // #1. for_each 알고리즘
     std::for_each(v.begin(), v.end(), foo);
+
+
+    // #2. 일반 함수보다 람다 표현식을 권장합니다.
+
+    std::for_each(v.begin(), v.end(), 
+                [](int n) { std::cout << n << std::endl; } );
 
 
 	return 0;
