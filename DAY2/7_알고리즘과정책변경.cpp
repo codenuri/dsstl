110ec129035a4e4157305015773c4944e5429bc4
ecourse <smkang@codenuri.co.kr>
2024-08-06 15:18:41 +0900

    Add files via upload

--- DAY2/7_알고리즘과정책변경.cpp
+++ DAY2/7_알고리즘과정책변경.cpp
@@ -6,10 +6,21 @@
 #include <functional>
 #include "show.h"
 
+// <algorithm> 헤더
+// => 정렬, 검색, 순열, 이진 검색등의 "컴퓨터 알고리즘" 관련 기능
+
+// <numeric> 헤더
+// => 합, 부분합, 내적 등의 수치 관련 기능
+
+// <memory>
+// => 메모리 관련, 요소 초기화등..
+
 int main()
 {
 	std::vector<int> v1 = { 1,2,3,4,5,6,7,8,9,10 };
 
-	std::sort(v1.begin(), v1.end());
+	int n1 = std::accumulate(v1.begin(), v1.end(), 0);
+
+	std::cout << n1 << std::endl;
 
 }
