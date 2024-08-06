92b14782b335bf6ee7fe32cdcafe9bf1ebb723a3
ecourse <smkang@codenuri.co.kr>
2024-08-06 10:26:33 +0900

    Add files via upload

--- DAY2/3_algorithm5.cpp
+++ DAY2/3_algorithm5.cpp
@@ -12,6 +12,15 @@ int main()
 	auto p = std::remove(v.begin(), v.end(), "AA");
 
 	show(v); // 결과 예측해 보세요
+			// 1. BB, CC, AA, CC
+	        // 2. BB, CC, , 
+			// 3. BB, CC, AA,   <= 정답
+
+	// 위 코드를 이해 하려면 std::move 알아야 합니다.
+	std::string s1 = "AA";
+	std::string s2 = "AA";
+	std::string s3 = s1;
+	std::string s4 = s2;
 }
 
 
