394ac116906693f76a00d342ec8e533fbb1a50d1
ecourse <smkang@codenuri.co.kr>
2024-08-06 12:15:00 +0900

    Add files via upload

---
+++ DAY2/임시객체2.cpp
@@ -0,0 +1,14 @@
+class Point
+{
+	int x, y;
+public:
+	Point(int x, int y) : x(x), y(y) {}
+};
+
+void draw_line(const Point& from, const Point& to) {}
+
+int main()
+{
+	// draw_line 을 사용해서 (1, 1) ~ (10,10) 에 선을 그려보세요
+
+}
