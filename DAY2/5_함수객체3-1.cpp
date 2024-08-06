1a219bab928853bcc4122e47ea3bb64805cd416b
ecourse <smkang@codenuri.co.kr>
2024-08-06 12:47:00 +0900

    Add files via upload

--- DAY2/5_함수객체3-1.cpp
+++ DAY2/5_함수객체3-1.cpp
@@ -22,13 +22,19 @@ int main()
 
 	int k = 3; 
 
-	IsDivide f(k);
-	bool b = f(10); 
+	IsDivide f(k);  // f 라는 객체는 내부 멤버 데이타로 "k값보관"
+	bool b = f(10); // 10 % value(k값)
+					// => 결국 f 는 "k값을 보관"한 단항 조건자. 
 
-	auto ret1 = std::find_if(s.begin(), s.end(), foo);
+//	auto ret1 = std::find_if(s.begin(), s.end(), f);
 
+	auto ret1 = std::find_if(s.begin(), s.end(), IsDivide{k} );
 
+	// => 위코드는 결국 "IsDidive 단항함수 객체와 main 지역변수 k"
+	//    를 같이 전달하는 코드
 
+	// Closure : 자신이 포함된 문맥의 지역변수를 캡쳐할수 있는
+	//			 능력이 있는 함수(객체)
 
 }
 
