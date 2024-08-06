360603163c33fda38daa129d5c139efacc78f017
ecourse <smkang@codenuri.co.kr>
2024-08-06 15:32:55 +0900

    Add files via upload

--- DAY2/7_알고리즘과정책변경2.cpp
+++ DAY2/7_알고리즘과정책변경2.cpp
@@ -12,9 +12,16 @@ int main()
 	std::vector<int> v1 = { 1,2,3,4,5,6,7,8,9,10 };
 	std::vector<int> v2;
 
+//	std::partial_sum(v1.begin(), v1.end(), std::back_inserter(v2));
+								// => 기본 버전은 + 연산자 사용
 
-	std::partial_sum(v1.begin(), v1.end(), std::back_inserter(v2));
+	// * 를 사용한 부분합을 구해 보세요
+//	std::partial_sum(v1.begin(), v1.end(), std::back_inserter(v2),
+//									std::multiplies<int>{} );
 
+	// 근접차
+	std::adjacent_difference(v1.begin(), v1.end(),
+							std::back_inserter(v2));
 
 	show(v2);
 }
