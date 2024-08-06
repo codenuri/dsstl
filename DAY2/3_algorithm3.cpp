b50b25a7d4b671a9b0f0a1304601a2a588008cc2
ecourse <smkang@codenuri.co.kr>
2024-08-06 10:12:04 +0900

    Add files via upload

--- DAY2/3_algorithm3.cpp
+++ DAY2/3_algorithm3.cpp
@@ -8,13 +8,17 @@
 
 int main()
 {
-	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
+//	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
+	std::list<int> v = { 1,2,3,1,2,3,1,2,3,1 };
 
-	auto p = std::remove(v.begin(), v.end(), 3);
+	// v 가 list 라도 아래 코드는 문제 없이 동작합니다.
+	// => 그런데, 성능을 생각해 보세요
+//	auto p = std::remove(v.begin(), v.end(), 3);
+//	show(v);
+//	v.erase(p, v.end());
 
-	show(v);
-
-	v.erase(p, v.end());
+	// list 는 멤버 함수 remove 가 있습니다.
+	v.remove(3); // 3을 찾을때 마다 제거.
 
 	show(v);
 
