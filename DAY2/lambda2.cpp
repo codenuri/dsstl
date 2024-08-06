5edc85670f74e37d2696901a9b5f1ac0cc1c5628
ecourse <smkang@codenuri.co.kr>
2024-08-06 14:18:34 +0900

    Add files via upload

---
+++ DAY2/lambda2.cpp
@@ -0,0 +1,25 @@
+// lambda2.cpp
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional> 
+
+int main()
+{
+	// 람다 활용
+	// #1. std::sort() 등의 알고리즘에 인자로 전달
+	// #2. auto 변수에 담아서 함수 처럼 사용(인라인 함수와 유사)
+
+	? f = [](int a, int b) { return a + b; };
+
+
+}
+
+
+
+
+
+
+
+
+
