5e6b61ba2c5ea90c67bd489ecbd2dd3f6458e2b1
ecourse <smkang@codenuri.co.kr>
2024-08-07 13:24:42 +0900

    Add files via upload

---
+++ DAY3/4_사용자정의타입3-1.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <algorithm>
+#include "show.h"
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a = 0, int b = 0) : x(a), y(b) {}
+
+	void dump() const { std::cout << x << ", " << y << std::endl; }
+
+	bool operator<(const Point& other) const
+	{
+		return x < other.x;
+	}
+
+	bool operator==(const Point& other) const
+	{
+		return x == other.x && y == other.y;
+	}
+
+};
+
+int main()
+{
+	std::vector<Point> v;
+
+	v.emplace_back(1, 2); // Point(1,2)
+	v.emplace_back(2, 3); // Point(2,3)
+	v.emplace_back(3, 2);
+	v.emplace_back(0, 1);
+	
+	// 해결책 #3. Point 타입 자체에 "<" 와 "==" 연산자를 제공하면됩니다.
+	
+	std::sort( v.begin(), v.end() );
+
+}
+
+
+
+
