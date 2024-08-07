923c08f49487342a0aeea089ba668fc04f02f789
ecourse <smkang@codenuri.co.kr>
2024-08-07 14:23:25 +0900

    Add files via upload

---
+++ DAY3/5_array3.cpp
@@ -0,0 +1,18 @@
+#include <array>
+
+int main()
+{
+	std::array<int, 5> a = { 1,2,3,4,5 };
+
+	// #1. "raw array" 과 완전히 동일한 메모리 구조
+	// #2. 성능도 거의 유사
+	// #3. 하지만 멤버 함수가 있어서 "raw array" 보다는 편리
+	// #4. 삽입/삭제, 크기 변경등은 할수 없습니다.
+
+	auto sz = a.size();
+	auto first = a.begin();
+
+	a.push_back(10); // error
+	a.resize(10); // error
+
+}
