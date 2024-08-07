79ec68fcfd569c6c1ac40b3afe2af476263327d2
ecourse <smkang@codenuri.co.kr>
2024-08-07 13:43:37 +0900

    Add files via upload

--- DAY3/spaceship1.cpp
+++ DAY3/spaceship1.cpp
@@ -6,6 +6,8 @@ int main()
 	int a = 1;
 	int b = 2;
 
+	// <=> : three way comparison operator 라는 공식이름
+	//		 "spaceship operator" 라는 별명!
 	auto ret = a <=> b;
 
 	if (ret < 0)         std::cout << "a < b\n";
--- DAY3/spaceship2.cpp
+++ DAY3/spaceship2.cpp
@@ -3,19 +3,34 @@
 class Point
 {
 	int x = 0;
+	int y = 0;
 public:
 
 	// 컴파일러가 기본 구현 제공해 달라
-	auto operator<=>(const Point& other) = default;
+	auto operator<=>(const Point& other) const = default;
+
+	// 컴파일러가 만드는 코드
+	/*
+	auto operator<=>(const Point& other) const
+	{
+		auto ret = x <=> other.x;
+
+		if (ret == 0)
+			return y <=> other.y;
+		
+		return ret;
+	}
+	*/
 };
 
 int main()
 {
 	Point p1, p2;
 
-	p1 < p2;
-	p1 > p2;
-	p1 == p2;
+	// 사용자코드	// 컴파일러가 변경한 코드(rewrite expression)
+	p1 < p2;		// (p1 <=> p2) < 0
+	p1 > p2;		// (p1 <=> p2) > 0
+	p1 == p2;		// (p1 <=> p2) == 0
 	p1 != p2;
 	p1 <= p2;
 	p1 >= p2;
