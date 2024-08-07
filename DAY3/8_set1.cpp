2df170c3caf69e0612d58da53e14182f330c427f
ecourse <smkang@codenuri.co.kr>
2024-08-07 16:21:50 +0900

    Add files via upload

---
+++ DAY3/8_set1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include "show.h"
+
+#include <set> // Red Black Tree·Î ±¸Çö
+
+int main()
+{
+	std::vector<int> v = { 20, 30, 15, 5, 40 };
+
+
+	std::set<int> s;
+
+	s.insert(20);
+	s.insert(30);
+	s.insert(15);
+	s.insert(5);
+	s.insert(40);
+
+	auto p = s.begin();
+
+	while (p != s.end())
+	{
+		std::cout << *p << std::endl;
+		++p;
+	}
+	
+}
+
+
+
+
+
+
+
