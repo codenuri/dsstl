9b6625f0ca807881dc8624d7da16e1f631c362bc
ecourse <smkang@codenuri.co.kr>
2024-08-07 14:34:13 +0900

    Add files via upload

--- DAY3/6_컨테이너어답터1.cpp
+++ DAY3/6_컨테이너어답터1.cpp
@@ -9,18 +9,31 @@
 // 1. 새롭게 만들자.
 // 2. list를 스택처럼 보이게 하자.
 
-template<typename T>
+// adapter 패턴 : 기존 클래스의 함수이름을 변경해서
+//				 다른 클래스 처럼 보이게 하는 디자인 패턴
+
+// 아래 코드는 list 의 멤버함수(push_back) 을
+//            stack 처럼 보이게 변경하는 어답터
+
+template<typename T, typename C = std::deque<T> >
 class stack
 {
-	std::list<int> c;
+	C c;
 public:
-	void push(const T& e) { c.push_back(e); }
-	void pop()			  { c.pop_back(); }
-	T&   top()			  { return c.back(); }
+	inline void push(const T& e) { c.push_back(e); }
+	inline void pop()			 { c.pop_back(); }
+	inline T&   top()			 { return c.back(); }
 };
 
 int main()
 {
+	stack<int> s1;  // 내부적으로 std::deque 사용
+	stack<int, std::vector<int>> s2;
+					// vector 를 stack 처럼 보이게 해달라는 의미
+	
+	stack<int, std::list<int>> s3;
+
+
 	stack<int> s;
 	s.push(10);
 	s.push(20);
