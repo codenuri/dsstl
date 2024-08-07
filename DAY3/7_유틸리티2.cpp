6515e0ee27613359511e5e157f7c9661747e7902
ecourse <smkang@codenuri.co.kr>
2024-08-07 15:45:30 +0900

    Add files via upload

--- DAY3/7_유틸리티2.cpp
+++ DAY3/7_유틸리티2.cpp
@@ -7,7 +7,16 @@ int main()
 	std::pair<int, double> p1(3, 3.4);
 
 	// pair 에 3개 보관가능합니다
-	std::pair<int, std::pair<double, char>> p2(1, { 3.4, 'A' } );
+	std::pair<int, std::pair<double, char> > p2(1, { 3.4, 'A' } );
 
-	std::cout << ? << std::endl; // 'A' 출력해 보세요
+	std::cout << p2.second.second << std::endl; // 'A' 출력해 보세요
+
+	
+	// C++11 부터 pair 를 선형적으로 사용할수 있는 tuple 도입
+	std::tuple<int, double, char> t1(1, 3.4, 'A');
+
+
+	// 요소 접근은 std::get
+
+	double d = std::get<1>(t1); // 3.4
 }
