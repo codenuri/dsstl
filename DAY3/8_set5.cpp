58e86a23531782d682efffe425378c4ae2f0adc7
ecourse <smkang@codenuri.co.kr>
2024-08-07 17:04:12 +0900

    Add files via upload

--- DAY3/8_set5.cpp
+++ DAY3/8_set5.cpp
@@ -8,9 +8,11 @@ struct Point
 };
 
 // Point 객체 2개의 크기를 비교하는 함수 객체
+// => 함수 객체 만들때 "반드시 상수멤버함수" 가 되어야 합니다.
 struct Compare
-{
-	bool operator()(const Point& p1, const Point& p2)
+{ 
+//	bool operator()(const Point& p1, const Point& p2) // error
+	bool operator()(const Point& p1, const Point& p2) const 
 	{
 		return p1.x < p2.x;
 	}
