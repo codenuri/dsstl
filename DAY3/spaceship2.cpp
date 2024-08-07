89953b53553edd13d97bd1f9457ef5f292ad93c9
ecourse <smkang@codenuri.co.kr>
2024-08-07 14:09:18 +0900

    Add files via upload

--- DAY3/spaceship2.cpp
+++ DAY3/spaceship2.cpp
@@ -7,10 +7,9 @@ class Point
 public:
 
 	// 컴파일러가 기본 구현 제공해 달라
-	auto operator<=>(const Point& other) const = default;
+//	auto operator<=>(const Point& other) const = default;
 
 	// 컴파일러가 만드는 코드
-	/*
 	auto operator<=>(const Point& other) const
 	{
 		auto ret = x <=> other.x;
@@ -20,7 +19,13 @@ public:
 		
 		return ret;
 	}
-	*/
+
+	// <=> 를 디폴트 요청하면 : == 필요 없습니다.
+	// <=> 를 사용자가 만들면 : == 도 필요
+	bool operator==(const Point& other) const
+	{
+		return x == other.x;
+	}
 };
 
 int main()
@@ -34,4 +39,7 @@ int main()
 	p1 != p2;
 	p1 <= p2;
 	p1 >= p2;
+
+	std::string s1 = "hello";
+	std::string s2 = "hello";
 }
