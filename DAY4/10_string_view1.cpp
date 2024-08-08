83f5a6ea1ec49cacf51a899a56d8b82959567eb5
ecourse <smkang@codenuri.co.kr>
2024-08-08 17:06:41 +0900

    Add files via upload

---
+++ DAY4/10_string_view1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// 문자열 배열 vs 문자열 포인터
+
+int main()
+{
+	// 아래 2줄의 차이점을 생각해 보세요. 
+	// => 문자열이 있는 메모리 위치
+	// 
+	// #1. 메모리 구조
+	char sa[] = "hello";
+
+//	char* sp  = "hello"; // C 는 ok. C++ 은 에러
+	const char* sp = "hello"; // C++ 도 ok
+
+	*sa = 'a'; // ok
+//	*sp = 'a'; // runtime error. 상수메모리에 쓰기
+				// const char* 인 경우는 컴파일 에러.
+
+	// #2. 문자열 공유
+	char sa1[] = "hello";
+	char sa2[] = "hello";
+
+	const char* sp1 = "hello";
+	const char* sp2 = "hello";
+
+	printf("%p\n", sa1);
+	printf("%p\n", sa2);
+	printf("%p\n", sp1);
+	printf("%p\n", sp2);
+	
+	
+}
