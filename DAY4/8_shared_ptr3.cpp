331cadea06873c329637dff9e11ba539d6760daa
ecourse <smkang@codenuri.co.kr>
2024-08-08 15:18:48 +0900

    Add files via upload

--- DAY4/8_shared_ptr3.cpp
+++ DAY4/8_shared_ptr3.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 #include <memory>
-
+#include "show.h"
 void foo(void* p) { free(p); }
 
 int main()
 {
-	// 삭제자 변경
+	// #1. 아래 코드는 모두 "new" 로 메모리 할당
 	std::shared_ptr<int> sp1(new int);
 	std::shared_ptr<int> sp2 = std::make_shared<int>(0);
+
+
+	// #2. "new" 가 아닌 다른 방법으로 메모리 할당하기
+
+//	std::shared_ptr<int> sp3(static_cast<int*>( malloc(sizeof(int)));
+					// bug : 할당은 malloc, 해지는 : delete
+
+	// 방법 #1. 생성자 2번째 인자로 삭제자 전달
+	std::shared_ptr<int> sp3(static_cast<int*>(malloc(sizeof(int)),
+						   	 &foo); // 생성자 2번째 인자로 삭제자 전달
+									// 소멸자에서 delete 아닌 foo호출
+
+	std::shared_ptr<int> sp4(static_cast<int*>(malloc(sizeof(int)),
+		&free); // free를 직접 전달도 가능
+
+
+	// 방법 #2. allocator 전달
+	// => std::make_shared 가 아닌 std::allocate_shared
+
+	std::shared_ptr<int> sp5 =
+		std::allocate_shared<int>( debug_alloc<int>(), 0 );
 }
 
 
