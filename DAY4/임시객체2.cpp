1955a47b36d8b4555fda2f19431aa0fdd2618038
ecourse <smkang@codenuri.co.kr>
2024-08-08 13:27:49 +0900

    Add files via upload

--- DAY4/임시객체2.cpp
+++ DAY4/임시객체2.cpp
@@ -5,21 +5,23 @@ struct Counter
 	int cnt = 0;
 
 	Counter increment()
+//	Counter& increment()
 	{
 		++cnt;
+		std::cout << "increment : " << cnt << std::endl;
 		return *this;
 	}
 	~Counter()              { std::cout << "~Counter()\n"; }
 	Counter()               { std::cout << "Counter()\n"; }
-	Counter(const Counter&) { std::cout << "Counter(const Counter&)\n"; }
+	Counter(const Counter& other) : cnt(other.cnt) { std::cout << "Counter(const Counter&)\n"; }
 };
 
 int main()
 {
 	Counter c;
-//	c.increment();
-//	c.increment();
-//	c.increment();
+	c.increment();
+	c.increment();
+	c.increment();
 
 	std::cout << "---------------------------\n";
 	c.increment().increment().increment();
