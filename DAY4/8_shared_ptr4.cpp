88511ec4ddc041f92bb1016d69feccc6a6bb8571
ecourse <smkang@codenuri.co.kr>
2024-08-08 15:33:41 +0900

    Add files via upload

---
+++ DAY4/8_shared_ptr4.cpp
@@ -0,0 +1,26 @@
+#include "show.h"
+#include <memory>
+
+struct People
+{
+	std::string name;
+
+	People(const std::string& n) : name(n) {}
+
+	~People() { std::cout << name << " 파괴" << std::endl; }
+
+
+	std::shared_ptr<People> bf; // best friend
+};
+
+int main()
+{
+	std::shared_ptr<People> sp1(new People("kim"));
+	std::shared_ptr<People> sp2(new People("lee"));
+
+	// 아래 코드가 있으면 메모리 해지가 안됩니다.
+	sp1->bf = sp2;
+	sp2->bf = sp1;
+}
+
+
