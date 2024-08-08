a879718d9ec7d85b33d1b0ac7f3077641e542a33
ecourse <smkang@codenuri.co.kr>
2024-08-08 13:50:53 +0900

    Add files via upload

--- DAY4/7_smart_pointer4.cpp
+++ DAY4/7_smart_pointer4.cpp
@@ -2,6 +2,9 @@
 
 // #1. 스마트 포인터는 템플릿으로 만들게 됩니다.
 
+// #2. 얕은 복사 문제를 해결해야 합니다.
+// => 다음 소스에서 해결
+
 template<typename T>
 class Ptr
 {
@@ -18,7 +21,9 @@ int main()
 	Ptr<int> p1 = new int;
 	*p1 = 10;
 
-	Ptr<int> p2 = p1; //!!
+	Ptr<int> p2 = p1;	// 컴파일러가 제공하는 복사 생성자 사용
+						// Shallow Copy(얕은 복사) 문제 발생
+					  
 
 }
 
