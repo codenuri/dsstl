0eeef53055e73e42467c3c12fb42484df085e147
ecourse <smkang@codenuri.co.kr>
2024-08-09 08:49:57 +0900

    Add files via upload

--- DAY5/1_string_view2.cpp
+++ DAY5/1_string_view2.cpp
@@ -15,11 +15,18 @@ int main()
 	
 	printf("s   buffer : %p\n", s.data());
 	printf("ss1 buffer : %p\n", ss1.data());
-	printf("sv1 buffer : %p\n", sv1.data());
+	printf("sv1 buffer : %p\n", sv1.data()); // s.data() 와 동일
 
 
 
 	// #2. literal 초기화
 	std::string      ss2 = "practice make best";
+					// => 힙에 메모리를 할당해서
+					// => 문자열의 복사본을 생성합니다.
+					// => 즉, 문자열 소유
+
 	std::string_view sv2 = "practice make best";
+					// => 상수 메모리의 문자열을 가리키게 됩니다.
+					// => const char* 입니다.
+
 }
