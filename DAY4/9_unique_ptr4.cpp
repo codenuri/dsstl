ec4cb5f70999b408976218f1ba8e343df95594f0
ecourse <smkang@codenuri.co.kr>
2024-08-08 16:57:24 +0900

    Add files via upload

--- DAY4/9_unique_ptr4.cpp
+++ DAY4/9_unique_ptr4.cpp
@@ -24,22 +24,24 @@ int* Alloc1()
 {
 	return new int;
 }
-std::shared_ptr<int> Alloc2()
-{
-	return std::make_shared<int>(0);
-}
-std::unique_ptr<int> Alloc3()
-{
-	return std::make_unique<int>(0);
-}
+std::shared_ptr<int> Alloc2() {	return std::make_shared<int>(0);}
+std::unique_ptr<int> Alloc3() {	return std::make_unique<int>(0);}
+
 int main()
 {
 	int* p = Alloc1();
 	
 	// 에러를 찾으세요
-	std::shared_ptr<int> sp1 = Alloc2();
-	std::unique_ptr<int> up1 = Alloc2();
+	// => Alloc2, 3 는 모두 값 타입 반환
+	// => 임시객체 형태로 반환됨
+	// => 우변이 임시객체이면 복사가 아닌 "move" 됩니다.
+	std::shared_ptr<int> sp1 = Alloc2(); // std::move(shared) ok 
+	std::unique_ptr<int> up1 = Alloc2(); // std::move(shared) error
 
-	std::shared_ptr<int> sp2 = Alloc3();
-	std::unique_ptr<int> up2 = Alloc3();
+	std::shared_ptr<int> sp2 = Alloc3(); // std::move(unique) ok
+	std::unique_ptr<int> up2 = Alloc3(); // std::move(unique) ok
+
+	// Alloc2 처럼 만들면 : 호출자는 반드시 shared_ptr로만 받아야 합니다.
+	// Alloc3 처럼 만들면 : 호출자가 선택할수 있습니다.
+	//						=> 권장. 
 }
