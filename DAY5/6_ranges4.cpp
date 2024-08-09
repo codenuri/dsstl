b3c7ad5625a9f47c2544827956490f60ab9058d1
ecourse <smkang@codenuri.co.kr>
2024-08-09 15:31:34 +0900

    Add files via upload

--- DAY5/6_ranges4.cpp
+++ DAY5/6_ranges4.cpp
@@ -11,12 +11,20 @@ int main()
 	std::vector<int> v = { 1,2,3,7,8,9,4,5,6,10 };
 
 	// #1. 클래스 직접 사용
-	std::ranges::filter_view fv(v, [](int n) { return n % 2 == 0; });
-
-	std::ranges::take_view tv(fv, 3);
-
-	std::ranges::reverse_view rv(tv);
+	// => "std::ranges" 라는 이름의 namespace 
+//	std::ranges::filter_view fv(v, [](int n) { return n % 2 == 0; });
+//	std::ranges::take_view tv(fv, 3);
+//	std::ranges::reverse_view rv(tv);
 	
+
+	// #2. 함수 버전 사용
+	// => "std::views" 라는 이름의 namespace 
+	auto fv = std::views::filter(v, [](int n) { return n % 2 == 0; });
+
+	auto tv = std::views::take(fv, 3);
+
+	auto rv = std::views::reverse(tv);
+
 
 	for (auto e : rv) // 결과 예측해 보세요 4, 8, 2
 	{
