82235f48d20e8defdfbc0146ff733b2c03e0d166
ecourse <smkang@codenuri.co.kr>
2024-08-09 14:48:19 +0900

    Add files via upload

---
+++ DAY5/6_ranges1-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct Point3D
+{
+	int x = 1;
+	int y = 2;
+	int z = 3;
+};
+
+int main()
+{
+	Point3D p;
+
+	for (auto e : p)
+	{
+		std::cout << e << ", ";
+	}
+	// 빌드하면 에러 있습니다.
+	// => 에러 메세지 예측해 보세요
+	// => 그리고 확인해 보세요
+}
