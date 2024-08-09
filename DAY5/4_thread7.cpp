f694955ffd207fc25c83ef6cdac342cf77c5460f
ecourse <smkang@codenuri.co.kr>
2024-08-09 14:04:43 +0900

    Add files via upload

---
+++ DAY5/4_thread7.cpp
@@ -0,0 +1,30 @@
+#include <atomic>
+
+// 아주 어려운 주제!!
+int a = 0;
+int b = 0;
+
+// foo, goo 는 다른 스레드가 실행합니다.
+void foo()
+{
+	a = b + 1;
+
+	std::atomic_thread_fence(
+		std::memory_order_seq_cst);
+
+	b = 1;
+}
+
+void goo()
+{
+	if (b == 1)
+	{
+		// a는 반드시 1일까요 ?
+	}
+}
+
+int main()
+{
+}
+
+// godbolt.org 사이트 접속해 보세요
