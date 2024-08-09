d90debdabacb88fe79cd8a6035caaf3374df3c90
ecourse <smkang@codenuri.co.kr>
2024-08-09 15:29:18 +0900

    Add files via upload

--- DAY5/6_ranges4.cpp
+++ DAY5/6_ranges4.cpp
@@ -11,14 +11,14 @@ int main()
 	std::vector<int> v = { 1,2,3,7,8,9,4,5,6,10 };
 
 	// #1. 클래스 직접 사용
-	std::ranges::filter_view fv(v, [](int n) { return n % 2 == 0};);
+	std::ranges::filter_view fv(v, [](int n) { return n % 2 == 0; });
 
 	std::ranges::take_view tv(fv, 3);
 
 	std::ranges::reverse_view rv(tv);
 	
 
-	for (auto e : rv) // 결과 예측해 보세요
+	for (auto e : rv) // 결과 예측해 보세요 4, 8, 2
 	{
 		std::cout << e << ", ";
 	}
