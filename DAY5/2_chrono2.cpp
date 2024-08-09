b38423849aa0d8596e6a1d25b380227c9891cee1
ecourse <smkang@codenuri.co.kr>
2024-08-09 09:39:05 +0900

    Add files via upload

---
+++ DAY5/2_chrono2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <chrono>
+
+// duration 정의 : 특정 단위의 갯수(tick count)를 표현하는 자료구조
+// 
+//				   값하나을 보관하고, 
+//				   템플릿 인자로 값의 타입과 단위를 전달 받는 자료 구조
+// 
+//				  10초 => 1초의 10개
+//					   => 1/1000초의 10000개..
+
+int main()
+{
+	// 1. duration 사용
+	std::chrono::duration<int, std::ratio<1, 100>>  d1(2); // 1/100 * 2
+	std::chrono::duration<int, std::ratio<1, 1000>> d2(d1);
+
+	std::cout << d2.count() << std::endl; // 20
+
+
+	using Meter = std::chrono::duration<int, std::ratio<1, 1>>;
+	using CentiMeter = std::chrono::duration<int, ?>;
+	using KiloMeter  = std::chrono::duration<int, ? >;
+
+	KiloMeter km(1);
+
+	Meter m(km);
+	CentiMeter cm(m);
+
+	std::cout << m.count() << std::endl; // ?
+	std::cout << cm.count() << std::endl; // ?
+
+
+}
