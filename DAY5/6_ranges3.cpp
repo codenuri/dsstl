73686a412d0e22f17e139a81b3e3c2b7af7536fe
ecourse <smkang@codenuri.co.kr>
2024-08-09 15:20:21 +0900

    Add files via upload

--- DAY5/6_ranges3.cpp
+++ DAY5/6_ranges3.cpp
@@ -22,6 +22,24 @@ take_view<T> take(T& c, std::size_t cnt)
 	return take_view<T>(c, cnt);
 }
 
+//--------------------------------
+// pipe line 을 지원하는 위한 코드들
+struct take_helper
+{
+	std::size_t count;
+};
+
+take_helper take(std::size_t cnt)
+{
+	return take_helper{ cnt };  // 생성자 없어도 이렇게 초기화 가능
+}
+
+template<typename C> 
+take_view<C> operator|(C& c, take_helper helper)
+{
+	return take_view<C>(c, helper.count);
+}
+
 
 int main()
 {
@@ -33,9 +51,14 @@ int main()
 	// #2. 도움 함수(object generator)
 	auto tv2 = take(v, 5);
 
+	// #3. 핵심: pipe line 기술 적용
+	auto tv3 = v | take(5); // vector | take_helper
+							// operator|(vector, take_helper)
+					// take_view operator|(Container, take_helper)
+
 	
 
-	for (auto e : tv1)
+	for (auto e : tv3)
 	{
 		std::cout << e << ", ";
 	}
