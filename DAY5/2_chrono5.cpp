6f454f301a0efda04c84aa35a02d8a93f5fc6ca5
ecourse <smkang@codenuri.co.kr>
2024-08-09 10:27:53 +0900

    Add files via upload

--- DAY5/2_chrono5.cpp
+++ DAY5/2_chrono5.cpp
@@ -13,16 +13,21 @@ void ex1(int arg)
 class stop_watch
 {
 	std::chrono::system_clock::time_point start;
-
 public:
 	stop_watch()
 	{
-
+		start = std::chrono::system_clock::now();
 	}
 
 	~stop_watch()
 	{
+		std::chrono::system_clock::time_point end = 
+								std::chrono::system_clock::now();
 
+		// time_point 끼리 뺄셈을 하면 결과가 nanoseconds 타입으로 나오게됩니다.
+		std::chrono::nanoseconds nano = end - start;
+
+		std::cout << nano.count() << std::endl;
 	}
 };
 
