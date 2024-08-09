61a04e98aca854c6ad6b94c9556b314575d28761
ecourse <smkang@codenuri.co.kr>
2024-08-09 14:29:04 +0900

    Add files via upload

--- DAY5/5_traits2.cpp
+++ DAY5/5_traits2.cpp
@@ -5,6 +5,13 @@ template<typename T> struct is_pointer
 	enum { value = false };
 };
 
+// template 부분 특수화 문법
+// => 타입이 특정 조건인 경우 다른 구현을 제공하는 문법
+template<typename T> struct is_pointer<T*>
+{
+	enum { value = true };
+};
+
 
 template<typename T> void foo(const T& b)
 {
