023883395744697b58ea8804c29a4a87c4d16ed9
ecourse <smkang@codenuri.co.kr>
2024-08-09 09:16:29 +0900

    Add files via upload

--- DAY5/1_string_view5.cpp
+++ DAY5/1_string_view5.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-
+#include <string>
+using namespace std::literals; // 이 안에 
+							   // C++ 표준의 접미사리터럴이 있습니다.
+							   // s, sv, i, min, ns, ms, us
+			
 class Meter
 {
 	int value;
 public:
 	Meter(int n) : value(n) {}
 };
-Meter operator""m(int v)
+Meter operator""_m(unsigned long long v)
 {
-	Meter a(v);
+	Meter a( static_cast<int>(v) );
 	return a;
 }
-
 int main()
 {
-	Meter n = 3m;
+	Meter n = 3_m; // operator""m(3) 을 찾게 됩니다.
+
+	float b = 3.0f;
+
+	auto s = "hello"s; // std::string operator""s("hello")
 }
